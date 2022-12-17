08da44543b95db8a926f3798e17367b01ca206b8
pkonshik <prosh.konshik@gmail.com>
2022-12-17 05:35:19 +0300

    Moved functions to witcherItem, witcherActor classes

--- module/witcherActor.js
+++ module/witcherActor.js
@@ -1,3 +1,5 @@
+import { witcher } from "./config.js";
+
 export default class WitcherActor extends Actor {
   prepareData() {
     super.prepareData();
@@ -6,8 +8,115 @@ export default class WitcherActor extends Actor {
   async rollItem(itemId) {
     this.sheet._onItemRoll(null, itemId)
   }
-  
+
   async rollSpell(itemId) {
     this.sheet._onSpellRoll(null, itemId)
+  }
+
+  getControlledToken() {
+    let tokens = game.canvas.tokens.controlled.slice()
+    let token;
+    if (tokens.length == 0) {
+      if (game.user.character) {
+        token = game.user.character.token
+      } else {
+        return ui.notifications.error(game.i18n.localize("WITCHER.Context.SelectActor"));
+      }
+    } else {
+      token = tokens[0]
+    }
+
+    return token;
+  }
+
+  getSpeaker() {
+    return {
+      "alias": this.name,
+      "actor": this,
+      "scene": game.scenes.current,
+      "token": this.getControlledToken().document,
+    };
+  }
+
+  isEnoughThrowableWeapon(item) {
+    if (item.system.isThrowable) {
+      let throwableItems = this.items.filter(w => w.type == "weapon" && w.name == item.name);
+
+      let quantity = throwableItems[0].system.quantity >= 0 ?
+        throwableItems[0].system.quantity :
+        throwableItems.sum("quantity");
+      return quantity > 0
+    } else {
+      return false
+    }
+  }
+
+  getSubstance(name) {
+    return this.getList("component").filter(i => i.system.type == "substances" && i.system.substanceType == name);
+  }
+
+  getList(name) {
+    switch (name) {
+      case "alchemical":
+      case "armor":
+      case "component":
+      case "diagrams":
+      case "effect":
+      case "enhancement":
+      case "mount":
+      case "mutagen":
+      case "note":
+      case "profession":
+      case "race":
+      case "spell":
+      case "valuable":
+      case "weapon":
+        return this.items.filter(i => i.type == name)
+      default:
+        return ui.notifications.error(game.i18n.localize("WITCHER.err.NoItemsOfSpecificType"));
+    }
+  }
+
+  // Find needed component in the items list based on the component name or based on the exact name of the substance in the players compendium
+  // Components in the diagrams are only string fields.
+  // It is possible for diagram to have component which is actually the substance
+  // That is why we need to check whether specific component name could be a substance
+  // Ideally we need to store some flag (substances list for diagrams) to the diagram components
+  // which will indicate whether the component is substance or not.
+  // Such modification may require either modification dozens of compendiums, or some additional parsers
+  findNeededComponent(componentName) {
+    return this.items.filter(function (item) {
+      return item.type == "component" &&
+        (item.name == componentName ||
+          (item.system.type == "substances" &&
+            ((game.i18n.localize("WITCHER.Inventory.Vitriol") == componentName
+              && item.system.substanceType == "vitriol") ||
+              (game.i18n.localize("WITCHER.Inventory.Rebis") == componentName
+                && item.system.substanceType == "rebis") ||
+              (game.i18n.localize("WITCHER.Inventory.Aether") == componentName
+                && item.system.substanceType == "aether") ||
+              (game.i18n.localize("WITCHER.Inventory.Quebrith") == componentName
+                && item.system.substanceType == "quebrith") ||
+              (game.i18n.localize("WITCHER.Inventory.Hydragenum") == componentName
+                && item.system.substanceType == "hydragenum") ||
+              (game.i18n.localize("WITCHER.Inventory.Vermilion") == componentName
+                && item.system.substanceType == "vermilion") ||
+              (game.i18n.localize("WITCHER.Inventory.Sol") == componentName
+                && item.system.substanceType == "sol") ||
+              (game.i18n.localize("WITCHER.Inventory.Caelum") == componentName
+                && item.system.substanceType == "caelum") ||
+              (game.i18n.localize("WITCHER.Inventory.Fulgur") == componentName
+                && item.system.substanceType == "fulgur"))))
+    });
+  }
+
+  async removeItem(itemId, quantityToRemove) {
+    let foundItem = this.items.get(itemId)
+    let newQuantity = foundItem.system.quantity - quantityToRemove
+    if (newQuantity <= 0) {
+      await this.items.get(itemId).delete()
+    } else {
+      await foundItem.update({ 'system.quantity': newQuantity < 0 ? 0 : newQuantity })
+    }
   }
 }
--- module/witcherItem.js
+++ module/witcherItem.js
@@ -1,3 +1,5 @@
+import { witcher } from "./config.js";
+
 export default class WitcherItem extends Item {
   chatTemplate = {
     "weapon": "systems/TheWitcherTRPG/templates/partials/chat/weapon-chat.html"
@@ -7,15 +9,14 @@ export default class WitcherItem extends Item {
   }
 
   async createSpellVisualEffectIfApplicable(token) {
-    let spell = this;
-    if (spell.type == "spell" && token && spell.system.createTemplate) {
-      let distance = Number(spell.system.templateSize)
+    if (this.type == "spell" && token && this.system.createTemplate) {
+      let distance = Number(this.system.templateSize)
       let direction = 0
       let angle = 0
       let width = 1
-      switch (spell.system.templateType) {
+      switch (this.system.templateType) {
         case "rect":
-          distance = Math.hypot(Number(spell.system.templateSize))
+          distance = Math.hypot(Number(this.system.templateSize))
           direction = 45
           width = token.target.value;
           break;
@@ -25,7 +26,7 @@ export default class WitcherItem extends Item {
       }
 
       let effect = await canvas.scene.createEmbeddedDocuments("MeasuredTemplate", [{
-        t: spell.system.templateType,
+        t: this.system.templateType,
         user: game.user._id,
         distance: distance,
         direction: token.document.rotation - 90,
@@ -36,19 +37,149 @@ export default class WitcherItem extends Item {
         fillColor: game.user.color,
         angle: angle,
         width: width,
-        flags: { "witcher": { "origin": { "name": spell.name } } }
+        flags: {
+          "witcher": { "origin": { "name": this.name } },
+          "spell": this
+        }
       }], { keepId: true });
 
-      spell.visualEffectId = effect[0]._id;
+      this.visualEffectId = effect[0]._id;
     }
   }
 
   async deleteSpellVisualEffect() {
-    let spell = this;
-    if (spell.visualEffectId && spell.system.visualEffectDuration > 0) {
+    if (this.visualEffectId && this.system.visualEffectDuration > 0) {
       setTimeout(() => {
-        canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [spell.visualEffectId])
-      }, spell.system.visualEffectDuration * 1000);
+        canvas.scene.deleteEmbeddedDocuments("MeasuredTemplate", [this.visualEffectId])
+      }, this.system.visualEffectDuration * 1000);
     }
+  }
+
+  getItemAttackSkill() {
+    let alias = "";
+    switch (this.system.attackSkill) {
+      case "Brawling":
+        alias = game.i18n.localize("WITCHER.SkRefBrawling")
+        break;
+      case "Melee":
+        alias = game.i18n.localize("WITCHER.SkRefMelee");
+        break;
+      case "Small Blades":
+        alias = game.i18n.localize("WITCHER.SkRefSmall");
+        break;
+      case "Staff/Spear":
+        alias = game.i18n.localize("WITCHER.SkRefStaff");
+        break;
+      case "Swordsmanship":
+        alias = game.i18n.localize("WITCHER.SkRefSwordsmanship");
+        break;
+      case "Archery":
+        alias = game.i18n.localize("WITCHER.SkDexArchery");
+        break;
+      case "Athletics":
+        alias = game.i18n.localize("WITCHER.SkDexAthletics");
+        break;
+      case "Crossbow":
+        alias = game.i18n.localize("WITCHER.SkDexCrossbow");
+        break;
+      default:
+        break;
+    }
+
+    return {
+      "name": this.system.attackSkill,
+      "alias": alias
+    };
+  }
+
+  getAttackSkillFlags() {
+    return {
+      "witcher": { "origin": { "name": this.name } },
+      "attackSkill": this.system.attackSkill,
+      "item": this,
+    }
+  }
+
+  doesWeaponNeedMeleeSkillToAttack() {
+    if (this.system.attackSkill) {
+      //Check whether item attack skill is melee
+      //Since actor can throw bombs relying on Athletic which is also a melee attack skill
+      //We need specific logic for bomb throws
+      let meleeSkill = witcher.meleeSkills.includes(this.system.attackSkill)
+      let rangedSkill = witcher.rangedSkills.includes(this.system.attackSkill)
+
+      if (meleeSkill && rangedSkill) {
+        return meleeSkill && !this.system.usingAmmo && !this.system.isThrowable;
+      } else {
+        return meleeSkill;
+      }
+    }
+  }
+
+  isAlchemicalCraft() {
+    return this.system.alchemyDC && this.system.alchemyDC > 0;
+  }
+
+  async realCraft(roll) {
+    let craftMessage = {};
+
+    //if (roll.dice[0].results[0].result == 10) {
+    //  messageData.flavor += `<a class="crit-roll"><div class="dice-sucess"><i class="fas fa-dice-d6"></i>${game.i18n.localize("WITCHER.Crit")}</div></a>`;
+    //};
+    //if (roll.dice[0].results[0].result == 1) {
+    //  messageData.flavor += `<a class="crit-roll"><div class="dice-fail"><i class="fas fa-dice-d6"></i>${game.i18n.localize("WITCHER.Fumble")}</div></a>`;
+    //};
+    //roll.toMessage(messageData);
+
+    let craftDC = this.isAlchemicalCraft() ? this.system.alchemyDC : this.system.craftingDC;
+    let craftSuccess = roll._total > craftDC;
+
+    if (craftSuccess) {
+      craftMessage.flavor = game.i18n.localize("WITCHER.craft.ItemsSuccessfullyCrafted");
+    } else {
+      craftMessage.flavor = game.i18n.localize("WITCHER.craft.ItemsNotCrafted");
+    }
+    craftMessage.flavor += `<label><b> ${this.actor.name}</b></label><br/>`;
+
+    let craftedItemName;
+    if (this.system.associatedItem && this.system.associatedItem.name) {
+      let craftingComponents = this.system.craftingComponents.filter(c => Number(c.quantity) > 0);
+      craftingComponents.forEach(c => {
+        let componentsToDelete = this.actor.findNeededComponent(c.name);
+        let componentsCountToDelete = Number(c.quantity);
+        let componentsLeftToDelete = componentsCountToDelete;
+        let componentsCountDeleted = 0;
+
+        componentsToDelete.forEach(toDelete => {
+          let toDeleteCount = Math.min(Number(toDelete.system.quantity), componentsCountToDelete, componentsLeftToDelete);
+          if (toDeleteCount <= 0) {
+            return ui.notifications.info(`${game.i18n.localize("WITCHER.craft.SkipRemovalOfComponent")}: ${toDelete.name}`);
+          }
+
+          if (componentsCountDeleted < componentsCountToDelete) {
+            this.actor.removeItem(toDelete._id, toDeleteCount)
+            componentsCountDeleted += toDeleteCount;
+            componentsLeftToDelete -= toDeleteCount;
+            return ui.notifications.info(`${toDeleteCount} ${toDelete.name} ${game.i18n.localize("WITCHER.craft.ItemsSuccessfullyDeleted")} ${this.actor.name}`);
+          }
+        });
+
+        if (componentsCountDeleted != componentsCountToDelete || componentsLeftToDelete != 0) {
+          craftSuccess = false;
+          return ui.notifications.error(game.i18n.localize("WITCHER.err.CraftItemDeletion"));
+        }
+      });
+
+      if (craftSuccess) {
+        let craftedItem = { ...this.system.associatedItem };
+        Item.create(craftedItem, { parent: this.actor });
+        craftedItemName = craftedItem.name;
+      }
+    } else {
+      craftedItemName = game.i18n.localize("WITCHER.craft.SuccessfulCraftForNothing");
+    }
+
+    craftMessage.flavor += `<b>${craftedItemName}</b>`;
+    roll.toMessage(craftMessage);
   }
 }
